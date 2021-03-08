c0f1960a354bb48194e08f48d419f0062a7cdad1
Loredan David <davidloredanvicentiu@gmail.com>
2021-03-08 11:56:36 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+
+  /*
+
+    🏁 Please take this quick challenge and rename carefully all the variable, and fix this broken code by assigning the correct 
+    variable keyword you have learned in the quest.
+    ex: const name = "David";
+
+  */
+
+  const myName = "David";
+  console.log(myName)
+  const mySecondName = "Bruno";
+  console.log(mySecondName)
+  let description = "Hi, I'm David Bruno from SF, I like to cook and meet new people.";
+console.log(description)
+  const img1 = "http://www.go.com/davif.png";
+  console.log(img1)
+  let city = "San Francisco";
+   let abstract = "CA";
+   console.log(city)
+
+   let pplFollower = 109;
+   let peopleFollowing = 1500;
+console.log(peopleFollowing)
+
+  let whatTimeIsIt = new Date();
+  
+  
+  
+
+  /*
+
+    🏁 David is following one more account, increment the total of following account
+
+  */
+
